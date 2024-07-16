611863d9119e4fd654cc10adc1b8aa95035f2ebc
kitajusSus <116976902+kitajusSus@users.noreply.github.com>
2024-07-16 12:09:35 +0200

    Update 6. Group anagram.py

--- 6. Group anagram.py
+++ 6. Group anagram.py
@@ -1,1 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #default dictionary to store lists of anagrams
+        anagram = defaultdict(list) 
 
+        for s  in strs:
+            # Sorting the string and use it as the key
+            k = tuple(sorted(s))
+            # Append the original string to the corresponding list in the dictionary
+            anagram[k].append(s)
+        # return all the values from dictionary (lists of lists)
+        return list(anagram.values())
